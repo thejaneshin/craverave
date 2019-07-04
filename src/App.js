92d0ee8292343bc5809bfdb8875753eb4be60e1f
Jane Shin <thejaneshin@gmail.com>
2019-07-04 16:17:16 -0700

    results page now displays results

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import Location from './components/Location';
 import Price from './components/Price';
 import Type from './components/Type';
@@ -47,57 +46,45 @@ class App extends Component {
     this.setState({page: page});
   }
 
-  getRestaurants = () => {
-    const { location, prices, type } = this.state;
-    let pricesStr = prices.join();
-
-    axios.get('https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?', {
-      headers: {
-        Authorization: 'Bearer ' + process.env.REACT_APP_API_KEY
-      },
-      params: {
-        location: location,
-        ...(pricesStr ? {price: pricesStr} : {}),
-        ...(type ? {term: type} : {})
-      }
-
-    })
-    .then(response => {
-      const resResults = response.data.businesses
-      console.log(resResults);
-    })
-    .catch(err => console.log(err));
-  }
-
   render() {
     const { page, location, price1, price2, price3, price4, type } = this.state;
     return (
       <div className="App">
-        <article className="br3 ba b--black-10 mv5 mw6 shadow-5 center">
-          <main className="pa4 black-80 mt5">
             {
-              page === 'location'
-                ? <Location 
-                    onPageChange={this.onPageChange}
-                    onHandleInputChange={this.onHandleInputChange}
-                    location={location} />
+              page === 'results'
+                ? <Results state={this.state} />
                 : (
-                    page === 'price'
-                      ? <Price 
-                          onPageChange={this.onPageChange}
-                          onPriceChange={this.onPriceChange}
-                          price1={price1} price2={price2} price3={price3} price4={price4} />
-                      : (
-                          page === 'type'
-                            ? <Type onPageChange={this.onPageChange} 
+                    <article className="br3 ba b--black-10 mv5 mw6 shadow-5 center">
+                      <main className="pa4 black-80 mt5">
+                        {
+                          page === 'location'
+                            ? <Location 
+                                onPageChange={this.onPageChange}
                                 onHandleInputChange={this.onHandleInputChange}
-                                type={type} />
-                            : <Results getRestaurants={this.getRestaurants} />
-                        )
-                  )
+                                location={location} />
+                            : (
+                                page === 'price'
+                                  ? <Price 
+                                      onPageChange={this.onPageChange}
+                                      onPriceChange={this.onPriceChange}
+                                      price1={price1} price2={price2} price3={price3} price4={price4} />
+                                  : (
+                                      page === 'type'
+                                        ? <Type onPageChange={this.onPageChange} 
+                                            onHandleInputChange={this.onHandleInputChange}
+                                            type={type} />
+                                        : (
+                                            <div>
+                                              <h2>404</h2>
+                                            </div>
+                                          )
+                                    )
+                              )
+                        }
+                      </main>
+                    </article>  
+                  )   
             }
-          </main>
-        </article>  
       </div>
     );
   }
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,13 +1,67 @@
 import React from 'react';
+import axios from 'axios';
+import ReactLoading from 'react-loading';
 import './animate.css';
 
-const Results = ({ getRestaurants }) => {
-  getRestaurants()
-	return (
-		<div className="measure animated fadeIn">
-			<p className="f2 fw6 ph0 mh0">Results</p>
-		</div>
-	);
+class Results extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      businesses: [],
+      loading: true
+    }
+  }
+
+  componentDidMount() {
+    const { location, prices, type } = this.props.state;
+    let pricesStr = prices.join();
+
+    axios.get('https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?categories=restaurants,food&', {
+      headers: {
+        Authorization: 'Bearer ' + process.env.REACT_APP_API_KEY
+      },
+      params: {
+        location: location,
+        ...(pricesStr ? {price: pricesStr} : {}),
+        ...(type ? {term: type} : {})
+      }
+
+    })
+    .then(response => {
+      this.setState({
+        businesses: response.data.businesses,
+        loading: false
+      });
+
+    })
+    .catch(err => console.log(err));
+  }
+
+  render() {
+    const { loading, businesses } = this.state;
+    return (
+      <div className="measure animated fadeIn">
+        <p className="f2 fw6 ph0 mh0">Results</p>
+
+        <div>
+          {
+            loading === true
+              ? <ReactLoading type={'spinningBubbles'} color={'#42a5f5'}  height={'20%'} width={'20%'} />
+              : (
+                  <div>
+                    {businesses.map((business, index) => {
+                      return (
+                        <p key={index}><img src={business.image_url} alt={business.name} height="100%" width="100%"/><a href={business.url} target="_blank" rel="noopener noreferrer">{business.name}</a> {business.rating} {business.review_count}</p>)
+                    })}
+                  </div>
+                )
+          }
+        </div>
+      </div>
+    );
+  }
+
+
 }
 
 export default Results;

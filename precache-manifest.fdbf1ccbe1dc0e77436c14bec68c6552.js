b17d27a79c0c0ab5f4af4efb03087c21fb007680
Jane Shin <thejaneshin@gmail.com>
2019-07-07 15:39:55 -0700

    Updates

---
+++ precache-manifest.fdbf1ccbe1dc0e77436c14bec68c6552.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "735483bd5cecfdac082f26279e67902a",
+    "url": "/craverave/index.html"
+  },
+  {
+    "revision": "cc7f55c8c12e98868308",
+    "url": "/craverave/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "ba763ad78d459ecdd0dd",
+    "url": "/craverave/static/css/main.27e6c0fa.chunk.css"
+  },
+  {
+    "revision": "cc7f55c8c12e98868308",
+    "url": "/craverave/static/js/2.8fa9582a.chunk.js"
+  },
+  {
+    "revision": "ba763ad78d459ecdd0dd",
+    "url": "/craverave/static/js/main.12c873b3.chunk.js"
+  },
+  {
+    "revision": "f4ebdef697a565fe4121",
+    "url": "/craverave/static/js/runtime~main.17140894.js"
+  },
+  {
+    "revision": "a702299a5a9fbb4b95ee6748627871d9",
+    "url": "/craverave/static/media/header.a702299a.png"
+  }
+]);
